8899d58ac2d097fd555acff049e77bec1d8ebe83
Jithesh <jitheshmaroli555@gmail.com>
2024-12-27 23:56:12 +0530

    some changes on error handling

--- server.js
+++ server.js
@@ -57,6 +57,28 @@ app.use(
   })
 );
 
+// // Catch 404 and forward to error handler
+// app.use((req, res, next) => {
+//   const err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+
+// // Error handler
+// // eslint-disable-next-line no-unused-vars
+// app.use((err, req, res, next) => {
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   const statusCode = err.status || 500;
+//   res.status(statusCode);
+  
+//   if (statusCode === 404) {
+//     res.render('errors/404');
+//   } else {
+//     res.render('errors/500');
+//   }
+// });
 
 // Passport middleware
 app.use(passport.initialize());
